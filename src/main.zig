85c41d162f27fc4c2f71eb40239b907808bbcdfa
GigaGrunch <gigagrunch@gmail.com>
2024-03-14 17:06:00 +0100

    deserialize session

--- src/main.zig
+++ src/main.zig
@@ -11,6 +11,8 @@ pub fn main() !void {
     arena = arena_impl.allocator();
     defer arena_impl.deinit();
 
+    try deserialize();
+
     var args_it = try std.process.argsWithAllocator(arena);
     _ = args_it.next().?; // first arg is the exe's name
     const command = args_it.next() orelse {
@@ -32,6 +34,11 @@ fn executeStartCommand(args_it: *std.process.ArgIterator) !void {
     };
     var project = findProject(project_id) orelse {
         std.debug.print("Project with given ID not found: {s}\n", .{project_id});
+        std.debug.print("Known IDs:", .{});
+        for (projects) |project| {
+            std.debug.print(" {s}", .{project.id});
+        }
+        std.debug.print("\n", .{});
         return;
     };
 
@@ -86,12 +93,70 @@ fn serialize() !void {
     try std.fs.cwd().writeFile("ztracker_session.ini", text.items);
 }
 
+fn deserialize() !void {
+    const text = try std.fs.cwd().readFileAlloc(gpa, "ztracker_session.ini", 1024 * 1024 * 1024);
+    defer gpa.free(text);
+
+    var projects_list = std.ArrayList(Project).init(arena);
+
+    var block_it = std.mem.split(u8, text, "\n\n");
+    const version_block = block_it.next().?;
+    if (!std.mem.startsWith(u8, version_block, "version: 1")) return error.UnsupportedVersion;
+
+    while (block_it.next()) |project_block| {
+        var project: Project = undefined;
+
+        var line_it = std.mem.split(u8, project_block, "\n");
+        while (line_it.next()) |line| {
+            if (getTrimmedValue(line, "id")) |id| {
+                var id_copy = try arena.alloc(u8, id.len);
+                @memcpy(id_copy, id);
+                project.id = id_copy;
+            } else if (getTrimmedValue(line, "name")) |name| {
+                var name_copy = try arena.alloc(u8, name.len);
+                @memcpy(name_copy, name);
+                project.name = name_copy;
+            } else if (getTrimmedValue(line, "entries_len")) |len_str| {
+                const entries_len = try std.fmt.parseInt(usize, len_str, 10);
+                project.entries = try arena.alloc(Entry, entries_len);
+            } else if (getRawValue(line, "entries")) |entries_raw| {
+                for (project.entries, 0..) |*entry, i| {
+                    const entry_start = i * @sizeOf(Entry);
+                    const entry_end = entry_start + @sizeOf(Entry);
+                    var array: [@sizeOf(Entry)]u8 = undefined;
+                    @memcpy(&array, entries_raw[entry_start..entry_end]);
+                    entry.* = std.mem.bytesToValue(Entry, &array);
+                }
+            }
+        }
+
+        try projects_list.append(project);
+    }
+
+    projects = projects_list.items;
+}
+
+fn getRawValue(line: []const u8, comptime name: []const u8) ?[]const u8 {
+    const prefix = name ++ ": ";
+    if (std.mem.startsWith(u8, line, prefix)) {
+        return line[prefix.len..];
+    }
+    return null;
+}
+
+fn getTrimmedValue(line: []const u8, comptime name: []const u8) ?[]const u8 {
+    if (getRawValue(line, name)) |raw_value| {
+        return std.mem.trim(u8, raw_value, "\n\r\t ");
+    }
+    return null;
+}
+
 fn getMinutes(seconds: i64) u64 {
     return @intCast(@divFloor(seconds, 60));
 }
 
 fn findProject(id: []const u8) ?*Project {
-    for (&projects) |*project| {
+    for (projects) |*project| {
         if (std.mem.eql(u8, id, project.id)) {
             return project;
         }
@@ -99,13 +164,7 @@ fn findProject(id: []const u8) ?*Project {
     return null;
 }
 
-var projects = [_]Project {
-    .{
-        .id = "rnd",
-        .name = "R&D",
-        .entries = &.{},
-    },
-};
+var projects: []Project = undefined;
 
 const Project = struct {
     id: []const u8,
