84f3b594b85b4aaeb6d038af0da1c116fb5e0be0
GigaGrunch <gigagrunch@gmail.com>
2024-03-14 17:05:57 +0100

    global allocators

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,17 @@
 const std = @import("std");
 
+var gpa: std.mem.Allocator = undefined;
+var arena: std.mem.Allocator = undefined;
+
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
+    var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
+    gpa = gpa_impl.allocator();
+    defer _ = gpa_impl.deinit();
+    var arena_impl = std.heap.ArenaAllocator.init(gpa);
+    arena = arena_impl.allocator();
+    defer arena_impl.deinit();
 
-    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
-    defer arena.deinit();
-
-    var args_it = try std.process.argsWithAllocator(arena.allocator());
+    var args_it = try std.process.argsWithAllocator(arena);
     _ = args_it.next().?; // first arg is the exe's name
     const command = args_it.next() orelse {
         std.debug.print("Usage: ztracker <command> [args...]\n", .{});
@@ -31,16 +35,41 @@ fn executeStartCommand(args_it: *std.process.ArgIterator) !void {
         return;
     };
 
-    const start_time = std.time.timestamp();
+    var entries = try arena.alloc(Entry, project.entries.len + 1);
+    @memcpy(entries[0..project.entries.len], project.entries);
+
+    var entry = &entries[project.entries.len];
+    project.entries = entries;
+
+    entry.* = Entry {
+        .start = std.time.timestamp(),
+        .end = std.time.timestamp(),
+    };
+
     while (true) {
-        const elapsed = std.time.timestamp() - start_time;
+        const now = std.time.timestamp();
+        const elapsed = now - entry.start;
         const seconds: u8 = @intCast(@mod(elapsed, 60));
-        const minutes: u8 = @intCast(@divFloor(elapsed, 60));
+        const minutes = getMinutes(elapsed);
         std.debug.print("{s}: {d}:{d:0>2}\r", .{project.name, minutes, seconds});
+
+        const entry_minutes = getMinutes(entry.end - entry.start);
+        if (entry_minutes != minutes) {
+            entry.end = now;
+            try serialize();
+        }
 
         const one_second = 1_000_000_000;
         std.time.sleep(one_second);
     }
+}
+
+fn serialize() !void {
+    std.debug.print("serialize!\n", .{});
+}
+
+fn getMinutes(seconds: i64) u64 {
+    return @intCast(@divFloor(seconds, 60));
 }
 
 fn findProject(id: []const u8) ?*Project {
@@ -53,10 +82,20 @@ fn findProject(id: []const u8) ?*Project {
 }
 
 var projects = [_]Project {
-    .{ .id = "rnd", .name = "R&D" },
+    .{
+        .id = "rnd",
+        .name = "R&D",
+        .entries = &.{},
+    },
 };
 
 const Project = struct {
     id: []const u8,
     name: []const u8,
+    entries: []Entry,
+};
+
+const Entry = struct {
+    start: i64,
+    end: i64,
 };
