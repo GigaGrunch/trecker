d88911a24777217656ff36dc8f8f522de6f0a3d7
GigaGrunch <gigagrunch@gmail.com>
2024-03-14 17:05:52 +0100

    simple command parsing

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,30 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer arena.deinit();
+
+    var args_it = try std.process.argsWithAllocator(arena.allocator());
+    _ = args_it.next().?; // first arg is the exe's name
+    const command = args_it.next() orelse {
+        std.debug.print("Usage: ztracker <command> [args...]\n", .{});
+        return;
+    };
+
+    if (std.mem.eql(u8, command, "start")) {
+        try executeStartCommand(&args_it);
+    } else {
+        std.debug.print("Unknown command: {s}\n", .{command});
+    }
+}
+
+fn executeStartCommand(args_it: *std.process.ArgIterator) !void {
+    const project_id = args_it.next() orelse {
+        std.debug.print("Usage: ztracker start <project_id>\n", .{});
+        return;
+    };
+    std.debug.print("project name: {s}\n", .{project_id});
 }
