2be76a4bfc6ec0647a7074c938b608a4f9a4e27e
GigaGrunch <gigagrunch@gmail.com>
2024-08-19 11:59:25 +0200

    simplify

--- build.zig
+++ build.zig
@@ -38,40 +38,23 @@ pub fn build(b: *std.Build) !void {
     run_step.dependOn(&run_cmd.step);
 }
 
-var git_commit_hash_buffer = [_]u8 {0} ** 40;
 fn parseGitCommitHash(b: *std.Build) []const u8 {
     const fallback = std.fmt.comptimePrint("unknown git commit hash", .{});
 
-    const head_path = b.pathFromRoot(".git/HEAD");
+    var git_root = b.build_root.handle.openDir(".git", .{}) catch return fallback;
+    defer git_root.close();
 
-    const head_file = std.fs.openFileAbsolute(head_path, .{}) catch return fallback;
-    var head_text_buffer: [1000]u8 = undefined;
-    const head_text_len = head_file.readAll(head_text_buffer[0..]) catch return fallback;
-    const head_text = head_text_buffer[0..head_text_len];
+    const head_text = git_root.readFileAlloc(b.allocator, "HEAD", 1000) catch return fallback;
 
     var head_text_it = std.mem.tokenize(u8, head_text, " \r\n");
-
     const head_key = head_text_it.next() orelse return fallback;
     if (!std.mem.eql(u8, head_key, "ref:")) return fallback;
-
     const head_value = head_text_it.next() orelse return fallback;
 
-    var ref_path_buffer = [_]u8 {0} ** 1000;
-    var ref_path_stream = std.io.fixedBufferStream(&ref_path_buffer);
-    ref_path_stream.writer().print(".git/{s}", .{head_value}) catch return fallback;
-    const ref_path = b.pathFromRoot(ref_path_buffer[0..]);
-
-    const ref_file = std.fs.openFileAbsolute(ref_path, .{}) catch return fallback;
-    var ref_text_buffer: [1000]u8 = undefined;
-    const ref_text_len = ref_file.readAll(ref_text_buffer[0..]) catch return fallback;
-    const ref_text = ref_text_buffer[0..ref_text_len];
+    const ref_text = git_root.readFileAlloc(b.allocator, head_value, 1000) catch return fallback;
 
     var ref_text_it = std.mem.tokenize(u8, ref_text, "\r\n");
     const ref_value = ref_text_it.next() orelse return fallback;
 
-    var git_commit_hash_stream = std.io.fixedBufferStream(&git_commit_hash_buffer);
-    git_commit_hash_stream.writer().print("{s}", .{ref_value}) catch return fallback;
-    return git_commit_hash_buffer[0..];
+    return b.allocator.dupe(u8, ref_value) catch return fallback;
 }
-
-// 9d74ed9bea9286c2f174995760c1a12f7b374589
